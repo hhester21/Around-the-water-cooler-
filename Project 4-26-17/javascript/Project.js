904e480ca6f0eb1d957064b67d146a36c6919ba3
Darrell Freeman <nclespaul@yahoo.com>
2017-04-26 23:36:38 -0500

    Project Files from 4-26-17

---
+++ Project 4-26-17/javascript/Project.js
@@ -0,0 +1,94 @@
+
+$('body').on('click', '.tab-content .checkbox', function () {
+    $(this).toggleClass('checked');
+    console.log(this);                
+});
+
+$(document).ready(function() {
+    $("#SelectAll").on("click", function () {
+        
+        //console.log("checkedoff: ", checkedoff);
+        //$("checkedoff").removeClass("checked");
+       // $('.active input[type=checkbox]').trigger("click");
+    });
+});
+
+
+// On clicking the "Save My Preferences" button, get the topics and subtopics that the user picked.
+$("#submit").on("click", function() {
+    var subtopics = [];
+    var i;
+    var category = [];
+    var subcategory = [];
+    var picks = [];
+    var finalPicks = [];
+    var subtopicCount = [];
+    var categories = [];
+    var subcategories = [];
+    var numSubMenus = [];
+    var counter = 0;
+
+// Form an array that holds the categories and subcategories selected as user preferences.
+    subtopics = $("div").filter(".checked");
+    for (i = 0; i < subtopics.length; i++) {
+        categories.push(subtopics[i].parentElement.attributes.value.nodeValue);
+        subcategories.push(subtopics[i].outerText);
+        category.push({"category": subtopics[i].parentElement.attributes.value.nodeValue});
+        subcategory.push({"subcategory": subtopics[i].outerText});
+        picks.push({"value": category, subcategory});
+        finalPicks.push(picks);
+    }        
+
+    console.log("categories: ", categories);
+    console.log("subcategories: ", subcategories);
+    console.log("subtopics: ", subtopics);
+    console.log("category: ", category);
+    console.log("subcategory: ", subcategory);
+    console.log("picks: ", picks);
+    console.log("finalPicks: ", finalPicks);
+
+    var t = {};
+    for (var i = 0, l = picks.length; i < l; i++)    {
+    t[picks[i].value[i].category] = !t[picks[i].value[i].category] ? 1 : t[picks[i].value[i].category] + 1;
+    }
+    for (var prop in t) {
+        numSubMenus.push(t[prop]);
+    }
+    console.log("t: ", t);
+    console.log("numSubMenus", numSubMenus);
+
+    // _.uniq is from the underscore.js library.
+    var sortedCategories = _.uniq(categories);
+    console.log("sortedCategories: ", sortedCategories);
+    console.log(Object.keys(t).length);
+
+    var counter = 0;
+    
+    // Create the <ul>, id="linklist", that will hold all of the "main topic" buttons.
+    $("#main").append($("<ul>").attr({"class": "nav navbar-nav dropdown-menu", "id": "linklist", "role": "menu"}));
+    
+    // Dynamically create a button for each of the main topics, and append them to #linklist.
+    for (var x = 0; x < sortedCategories.length; x++) {
+        $("#linklist")
+        .append($("<li>").attr("id", "topic" + [x])
+        .append($("<a>").attr({"tab-index": -1, "class": "dropdown-submenu", "id": "link" + [x], "href": "#"})
+        .text(sortedCategories[x]).append($("<span>").attr("class", "glyphicon glyphicon-chevron-right pull-right"))));    
+        console.log("x: ", x);  
+
+        // Dynamically create buttons for each of the checkbox subtopics, and append them to main topic buttons.
+        $("#sublinks").append($("<ul>").attr({"class": "nav navbar-nav dropdown-submenu", "id": "sublinklist" + [x], "role": "menu"}));
+        for (var z = 0; z < numSubMenus[x]; z++) {            
+                $("#sublinklist" + [x])
+                    .append($("<li>").attr("id", "subtopic" + [z])
+                    .append($("<a>").attr({"tab-index": 1, "class": "dropdown-menu", "id": "sublink" + [z], "href": "#"})
+                    .text(subcategory[counter].subcategory)));
+                counter++;
+                console.log("x: ", x);
+                console.log("z: ", z);
+                console.log("counter: ", counter);
+                console.log("subcategory[z].subcategory: ", subcategory[z].subcategory);
+                console.log("numSubMenus[z]: ", numSubMenus[z]);
+        } 
+    }   
+});
+    
